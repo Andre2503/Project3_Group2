81dfd34ef711281b5b805cc6c3ae1be26a188433
Zonia Quiroga <118328051+zquiroga@users.noreply.github.com>
2023-11-06 23:16:09 +0800

    Add files via upload

--- app_solution.py
+++ app_solution.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from flask import Flask, jsonify
 from sqlalchemy import create_engine
@@ -32,12 +33,13 @@ def welcome():
     """List all available API routes."""
     return (
         "Available Routes:<br/>"
-        "/api/v1.0/Industry<br/>"
-        "/api/v1.0/Ticker"
+        "/api/v1.0/industry_groups<br/>"
+        "/api/v1.0/top_ten<br/>"
+        "/api/v1.0/top_ten_historic<br/>"
     )
 
-@app.route("/api/v1.0/Industry")
-def get_industry():
+@app.route("/api/v1.0/industry_groups")
+def get_industry_groups():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
@@ -48,32 +50,72 @@ def get_industry():
     session.close()
 
     # Convert list of tuples into a normal list
-    all_industry = list(np.ravel(results))
+    all_industry_groups = list(np.ravel(results))
 
-    return jsonify(all_industry)
+    return jsonify(all_industry_groups)
 
-@app.route("/api/v1.0/Ticker")
-def get_ticker():
+@app.route("/api/v1.0/top_ten")
+def get_top_ten():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of ticker data"""
-    # Query all ticker data
-    results = session.query(top_ten.company_name, top_ten.industry_id, top_ten.Volume).all()
+    """Return a list of top_ten data"""
+    # Query all top_ten data
+    results = session.query(top_ten.ticker,top_ten.company_name, top_ten.market_cap, top_ten.industry_id).all()
 
     session.close()
 
+     # Convert list of tuples into a normal list
+    all_top_ten = list(np.ravel(results))
+
+    return jsonify(all_top_ten)
+
+
+@app.route("/api/v1.0/top_ten_historic")
+def get_top_ten_historic():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of top_ten_historic"""
+    # Reflejar la tabla top_ten_historic
+    top_ten_historic = Base.classes.top_ten_historic
+
+    # Query all top_ten_historic data
+    results = session.query(
+        top_ten_historic.id,
+        top_ten_historic.Ticker,
+        top_ten_historic.Date,
+        top_ten_historic.Open,
+        top_ten_historic.High,
+        top_ten_historic.Low,
+        top_ten_historic.Close,
+        top_ten_historic.Volume
+    ).all()
+
+    session.close()
+
+    # Convert list of tuples into a normal list
+    all_top_ten_historic = list(np.ravel(results))
+
+    return jsonify(all_top_ten_historic)
+
+
+
+
+
     # Create a list of dictionaries to store the data
-    all_ticker = []
-    for company_name, industry_id, volume in results:
-        ticker_dict = {
+    industry_name = []
+    for company_name, industry_id, market_cap in results:
+        industry_name_dict = {
             "company_name": company_name,
             "industry_id": industry_id,
-            "volume": volume
+            "market_cap": market_cap
         }
-        all_ticker.append(ticker_dict)
+        all_industry_groups.append( industry_name_dict)
 
-    return jsonify(all_ticker)
+    return jsonify(all_industry_group)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
