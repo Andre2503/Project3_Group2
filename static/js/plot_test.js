667caacc6006ca8c935a24d2ca402b3f78bbf1df
gpang98 <135190892+gpang98@users.noreply.github.com>
2023-11-11 14:58:00 +0800

    15th Commit

--- static/js/plot_test.js
+++
@@ -1,9 +0,0 @@
-// Fetch data from the API
-fetch('http://127.0.0.1:5000/api/v1.0/fundamental')
-  .then(response => response.json())
-  .then(data => {
-    console.log('Response from API:', data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
